b115070f169988c1e8730f8f822462da70125f82
AleCiria <ciriacontacto@gmail.com>
2023-10-08 20:02:41 -0600

    Add files via upload

---
+++ stim_list_T_2.js
@@ -0,0 +1,617 @@
+var stim_list_T_2 = [
+
+  {stimulus: ["armchair_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["bikini_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["binoculars_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["bottle_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["boxing_glove_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["clock_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["dice_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["fork_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["grater_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["hairdryer_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["jar_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["jewelry_box_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["poker_chips_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["racket_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["rolling_pin_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["roman_helmet_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["saw_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["scooter_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["tiara_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["weights_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"congruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["whelk_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["whistle_SIMILAR.jpg"],
+   tipo:"similar",
+   contexto:"incongruente",
+   correct_response: "v",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["airplane_belt.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["appointment_book.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["balloons.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["beach_umbrella.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["blackboard_eraser.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["cactus.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["chaplet.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["color_palette.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["dagger.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["emergency_exit_signal.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["fire_hydrant.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["hose.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["ice_skates.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["luggage_carrier_cart.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["magic_cube.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["scythe.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["shoe_box.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["soap_dispenser.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["stethoscope.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["thimble.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["tragedy_mask.jpg"],
+   tipo:"antiguo",
+   contexto:"congruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["trampoline.jpg"],
+   tipo:"antiguo",
+   contexto:"incongruente",
+   correct_response: "n",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["brocha_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["calcetas_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["casco_bici_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["cepillo_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["chupon_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["cup_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["engrapadora_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["googles_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["gorra_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["guante_cocina_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["licuadora_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["linterna_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["marco_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["muñeca_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["palita_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["pandero_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["pinzas_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["piña_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["silla_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["sombrero_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["tazon_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+  {stimulus: ["vela_NUEVO.jpg"],
+   tipo: "nuevo",
+   contexto:"sin_contexto",
+   correct_response: "b",
+   antiguo: "n",
+   similar:  "v",
+   nuevo: "b"
+},
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
