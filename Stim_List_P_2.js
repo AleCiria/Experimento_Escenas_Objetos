10c3b9a4bc530969fe598eb2fe8fde0e1c84561a
AleCiria <ciriacontacto@gmail.com>
2023-10-27 20:45:08 -0600

    Add files via upload
    agregué js de la practica así como estímulos de practica también

---
+++ Stim_List_P_2.js
@@ -0,0 +1,33 @@
+var Stim_List_P_2= [
+
+  {stimulus: ["beach_A.jpg", "beach_A_CON.jpg"],
+   correct_response: "n",
+   con: "n",
+   inc: "v",
+   tipo: "1"
+},
+
+  {stimulus: ["desert_B.jpg", "desert_B_INC.jpg"],
+   correct_response:"v",
+   con: "n",
+   inc: "v",
+   tipo: "2"
+
+},
+
+  {stimulus: ["skate_park_B.jpg","skate_park_B_CON.jpg"],
+   correct_response:"n",
+   con: "n",
+   inc: "v",
+   tipo:"1"
+},
+
+
+  {stimulus: ["cemetery_B.jpg","cemetery_B_INC.jpg"],
+   correct_response:"v",
+   con: "n",
+   inc: "v",
+   tipo:"2"
+},
+
+]
